0fed2c4302b38d5467de361d6367ee0652d41be1
takeshi-3 <g.jem.beleeem8000@gmail.com>
2021-03-07 13:10:55 +0900

    fix aspect ratio of thumbnail

--- components/workItem.js
+++ components/workItem.js
@@ -7,7 +7,7 @@ import { animated } from 'react-spring';
 const WorkItem = ({fields, id, animProps, onClick}) => {
     return (
         <animated.div className={styles.work} style={{...animProps}} onClick={(e) => onClick(e)}>
-            <div className={styles.work_thumb}><img src={fields.thumbnail.fields.file.url} /></div>
+            <div className={styles.work_thumb} style={{backgroundImage: `url(${fields.thumbnail.fields.file.url})`}}></div>
             <h3 className={styles.work_title}>{fields.title}</h3>
             <p className={styles.work_skills}>
             {fields.skills.map(skill => 
