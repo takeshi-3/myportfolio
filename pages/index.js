dd2cc0b9f9f214c93b467cc06dead9e8b5848d0a
takeshi-3 <g.jem.beleeem8000@gmail.com>
2021-06-13 11:00:29 +0900

    自己紹介を追加

--- pages/index.js
+++ pages/index.js
@@ -36,8 +36,8 @@ export const getServerSideProps = async () => {
 };
 
 const metaInfo = {
-    title: "デザイナーのfunachunです。 | funachun.me",
-    description: "デザイナーfunachunのポートフォリオサイトです。WEBデザイン、ブランディング、UIUXデザインから開発までお任せください。",
+    title: "デザイナーの船津武志です。 | funachun.me",
+    description: "デザイナー船津武志のポートフォリオサイトです。WEBデザイン、ブランディング、UIUXデザインから開発までお任せください。",
     image: "/images/ogp.jpg",
     type: "website",
     url: "https://funachun.me"
@@ -70,9 +70,9 @@ const Home = ({ works, gallery }) => {
             <Header title={metaInfo.title} description={metaInfo.description} image={metaInfo.image} type={metaInfo.type} url={metaInfo.url} />
             <div className={styles.home}>
                 <section className={styles.home_profile}>
-                    <h1 className={styles.home_profile_me}>funachun <span>– designer</span></h1>
+                    <h1 className={styles.home_profile_me}>Takeshi Funatsu <span>– designer</span></h1>
                     <p className={styles.home_profile_desc}>
-                        はじめまして。デザイナーのfunachunと申します。<br />
+                        はじめまして。デザイナーの船津武志と申します。<br />
                         UIUXデザインやグラフィックデザインを通して<br />
                         世の中をちょっぴりカッコよくしています。<br /><br />
                         「伝えるべき本質をありのまま引き出す」<br />
@@ -109,9 +109,21 @@ const Home = ({ works, gallery }) => {
                 </section> */}
 
                 <section className={styles.home_contact} id="contact" ref={contactRef}>
-                    <SectionTitle title="CONTACT" />
+                    <SectionTitle title="PROFILE" />
                     <div className={styles.home_contact_wrapper}>
                         <div className={styles.home_contact_cont}>
+                            <p className={styles.home_contact_title}>経歴</p>
+                            <p className={styles.home_contact_item}>2019年　群馬工業高等専門学校 専攻科 生産システム工学専攻卒業</p>
+                            <p className={styles.home_contact_item}>2021年　東京大学大学院 学際情報学府 先端表現情報学コース 修了</p>
+                            <p className={styles.home_contact_line}></p>
+                            <p className={styles.home_contact_title}>テーマ・これまでの制作</p>
+                            <p className={styles.home_contact_desc}>
+                                水の流れがデジタルメディアに干渉して、デジタル上の絵を不可逆なものとして変化させる作品「Meltal.」の制作や、空間情報を共有可能な遠隔コミュニケーションシステムのインタラクションデザインなど、
+                                既存のデジタルメディアに対して物理空間の性質を折り込む行為を通して、物理空間やデジタルメディアそのものの性質についての問いを追求してきた。<br />
+                                現在、事業会社のプロダクト・UIUXデザイナーとして勤務中 (個人としての制作も継続中)。
+                            </p>
+                            <p className={styles.home_contact_item}></p>
+                            <p className={styles.home_contact_line}></p>
                             <p className={styles.home_contact_title}>Email</p>
                             <p className={styles.home_contact_item}>g.jem.beleeem8000(at)gmail.com</p>
                             <p className={styles.home_contact_line}></p>
